3cc21abe733b75bb82f76fa7fec2be9c72d28066
josephchigiz <josephochego@gmail.com>
2023-08-18 12:34:20 +0530

    push tasks

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- script that describes (first_table) from the (hbtn_0c_0)database 
 
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='first_table';
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='first_table' AND TABLE_SCHEMA='hbtn_0c_0';

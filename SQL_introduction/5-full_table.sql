722151b42f24dd44ec1e41c102bbd9ef6bdfe001
josephchigiz <josephochego@gmail.com>
2023-08-18 11:17:12 +0530

    push tasks

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,3 @@
 -- script that describes (first_table) from the (hbtn_0c_0)database 
--- CREATE SCHEMA hbtn_0c_0;
 
--- SHOW DATABASES;
-
--- USE hbtn_0c_0;
-
--- CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
-
-SELECT * FROM information_schema.COLUMNS WHERE TABLE_NAME='first_table';
+SELECT * FROM information_schema.COLUMNS WHERE TABLE_NAME='first_table' AS table_info;

432ce1f81d8e72306c9ddcdb0083c78a2544f6dc
josephchigiz <josephochego@gmail.com>
2023-08-18 12:37:15 +0530

    push tasks

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,13 @@
 -- script that describes (first_table) from the (hbtn_0c_0)database 
+-- DROP DATABASE IF EXISTS hbtn_test_db_5;
+-- CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+-- USE hbtn_test_db_5;
+-- CREATE TABLE IF NOT EXISTS first_table (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(128),
+--     c CHAR(1),
+--     created_at DATE,
+--     PRIMARY KEY (id)
+-- );
 
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='first_table' AND TABLE_SCHEMA='hbtn_0c_0';
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='first_table' AND TABLE_SCHEMA='hbtn_test_db_5';

59d866d444a88e8c12c531baaafc6d6e3da7f7f8
josephchigiz <josephochego@gmail.com>
2023-08-18 11:22:06 +0530

    push tasks

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- script that describes (first_table) from the (hbtn_0c_0)database 
 
-SELECT * FROM information_schema.COLUMNS WHERE TABLE_NAME='first_table' AS table_info;
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='first_table';
---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Insert new values into a table 
+
+INSERT INTO TABLE first_table (id, name) VALUES (89, 'Holberton School');

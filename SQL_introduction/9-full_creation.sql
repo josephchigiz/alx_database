ddb7bc009745b97cd29112804d748b4ddb1132c1
josephchigiz <josephochego@gmail.com>
2023-08-18 12:18:54 +0530

    push tasks

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,7 @@
 -- create and populate table
 
+USE hbtn_0c_0;
+
 CREATE TABLE if NOT EXISTS 'second_table' (id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table (id, name, score) VALUES(

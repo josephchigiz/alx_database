c3c1bc97102734b9960ec6790f99a61664fbbbcb
josephchigiz <josephochego@gmail.com>
2023-08-18 11:01:54 +0530

    push tasks

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -7,7 +7,4 @@ USE hbtn_0c_0;
 
 -- CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
 
--- SELECT * FROM information_schema.COLUMNS
--- WHERE TABLE_NAME='first_table';
-
-DESCRIBE first_table;
+SELECT * FROM information_schema.COLUMNS WHERE TABLE_NAME='first_table';

3c573a75fb0d4234cc71ca105105be31da38a119
josephchigiz <josephochego@gmail.com>
2023-08-19 11:50:29 +0530

    push tasks

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,3 @@
 -- script that describes (first_table) from the (hbtn_0c_0)database 
 
-SELECT 
-    (first_tableCREATETABLE`first_table`(
-    `id`intNOTNULLAUTO_INCREMENT,
-    `name`varchar(128)DEFAULTNULL,
-    `c`char(1)DEFAULTNULL,
-    `created_at`dateDEFAULTNULL,
-    PRIMARYKEY(`id`)
-    )ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci)
-    ;
+SHOW CREATE TABLE first_table;

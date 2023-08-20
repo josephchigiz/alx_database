9b8bbf51e0f5a7aa8edffaf0f86895c67c09d261
josephchigiz <josephochego@gmail.com>
2023-08-20 23:31:18 +0530

    push tasks

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create Database, then a user and give the user read permissions only
+
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 to 'user_0d_2'@'localhost';

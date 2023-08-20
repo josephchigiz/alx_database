f15bff9b14a95d0025894fcd674d7f63d3f380a0
josephchigiz <josephochego@gmail.com>
2023-08-20 23:06:41 +0530

    push tasks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- script lists permissions for the listed users
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

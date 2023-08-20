8db3aa51230fdc9b6e6e6bfbd4d888e3a28dc169
josephchigiz <josephochego@gmail.com>
2023-08-20 23:26:23 +0530

    push tasks

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create user and give all privileges
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;

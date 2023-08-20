c288e7fc8401db0b84290e0c92cda84027e74145
josephchigiz <josephochego@gmail.com>
2023-08-21 01:21:28 +0530

    push tasks

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database
 
-SELECT cities.id, cities.name, states.name AS state_name 
+SELECT cities.id, cities.name, states.name 
 FROM cities, states 
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
